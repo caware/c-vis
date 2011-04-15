56f45dabaaa1cdd89e33dfd59d9df68b420e87bc
Chris Elsmore <elsmorian@gmail.com>
2011-04-15 11:04:08 +0100

    Tweaked indexgenerator code

--- scripts/indexgenerator.py
+++ scripts/indexgenerator.py
@@ -4,71 +4,75 @@
 # Generate an index file from a tree of json log files
 # Chris Elsmore (cce25)
 #
-# Usage: indexgenerator.py dir/containing/root/of/json/file/tree/
+# Usage: indexgenerator.py root/of/json/file/tree/ path/to/output/json/file
 #
 
 import os, sys, json
 
-
-filetowrite = 'sensorindex.json'
-
-totalfiles = 0
-#firstrun = True
-currentsensor = ''
-alreadyfound = False
-#sensorfiles = []
-elec = []
-tempsensor = {}
-
-
-for dirname, dirnames, filenames in os.walk('/usr/groups/building/meter-readings/meters.cl.cam.ac.uk/elec/'):
-    for filename in filenames:
-        if (filename[-5:] == ".json"):
-            readfile = open(os.path.join(dirname, filename), 'r').read()
-            jsonfile = json.loads(readfile)
-            
-            alreadyfound = False
-            #if we know about this sensor, add its readings only.
+if len(sys.argv) != 3 or sys.argv[1] == "help":
+    print "Usage: indexgenerator.py root/of/json/file/tree/ path/to/output/json/file"
+    sys.exit()
+else:
+    [dirtoread, filetowrite] = sys.argv[1:]
+    
+    print "Input: "+dirtoread+", Output: "+filetowrite
+    
+    totalfiles = 0
+    currentsensor = ''
+    alreadyfound = False
+    elec = []
+    tempsensor = {}
+    
+    
+    for dirname, dirnames, filenames in os.walk(dirtoread):
+        for filename in filenames:
+            if (filename[-5:] == ".json"):
+                readfile = open(os.path.join(dirname, filename), 'r').read()
+                jsonfile = json.loads(readfile)
                 
-            for sensor in elec:
+                alreadyfound = False
+                #if we know about this sensor, add its readings only.
+                
+                for sensor in elec:
                     print 'trying sensor ID:'+sensor['sensor']
                     if sensor['sensor'] == 'S-m' + jsonfile['label'].split(' ', 1)[0]:
                         sensor['readings'].append(filename)
                         print 'Sensor ID:'+sensor['sensor']+' already found, readings: '+str(sensor['readings'])
                         alreadyfound = True
                         break
-
-            if not alreadyfound:
-                print 'sensor '+'S-m' + str(jsonfile['label'].split(' ', 1)[0])+' not found, adding' 
-                #if this is a new sensor, add its readings to elec.
-                tempsensor = {}
-                tempsensor['sensor'] = 'S-m' + jsonfile['label'].split(' ', 1)[0]
-                tempsensor['path'] = jsonfile['path']
-                tempsensor['room'] = jsonfile['room']           
-                tempsensor['function'] = jsonfile['description']
-                tempsensor['readings'] = [filename]
-                #tempsensor['readings'].append(filename)
-                elec.append(tempsensor)
-                print 'New sensor found: '+str(tempsensor)
-                #print elec
-                tempsensor = {}
-
-            
-            #for sensor in elec:
-            #        print sensor['sensor']
-        
-            totalfiles = totalfiles + 1
-
-myjson = {}
-sensors = {}
-sensors['elec'] = elec
-myjson['sensors'] = sensors
-print '\n'
-#print 'Total # sensors: '+str(len(elec))
-print '\n'
-#print 'Total Files Parsed: '+str(totalfiles)
-print json.dumps(myjson)
-
-outputfile = open(filetowrite, 'w')
-outputfile.write(json.dumps(myjson))
-outputfile.close()
+                
+                if not alreadyfound:
+                    print 'sensor '+'S-m' + str(jsonfile['label'].split(' ', 1)[0])+' not found, adding' 
+                    #if this is a new sensor, add its readings to elec.
+                    tempsensor = {}
+                    tempsensor['sensor'] = 'S-m' + jsonfile['label'].split(' ', 1)[0]
+                    tempsensor['path'] = jsonfile['path']
+                    tempsensor['room'] = jsonfile['room']           
+                    tempsensor['function'] = jsonfile['description']
+                    tempsensor['readings'] = [filename]
+                    #tempsensor['readings'].append(filename)
+                    elec.append(tempsensor)
+                    print 'New sensor found: '+str(tempsensor)
+                    #print elec
+                    tempsensor = {}
+                
+                
+                #for sensor in elec:
+                #        print sensor['sensor']
+                
+                totalfiles = totalfiles + 1
+                
+    myjson = {}
+    sensors = {}
+    sensors['elec'] = elec
+    myjson['sensors'] = sensors
+    print '\n'
+    #print 'Total # sensors: '+str(len(elec))
+    print '\n'
+    #print 'Total Files Parsed: '+str(totalfiles)
+    print json.dumps(myjson)
+    
+    outputfile = open(filetowrite, 'w')
+    outputfile.write(json.dumps(myjson))
+    outputfile.close()
+    sys.exit()
