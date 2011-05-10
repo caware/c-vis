a767d1808db9657e6353ddeb630a58fe078fefd2
Chris Elsmore <elsmorian@gmail.com>
2011-05-10 17:33:35 +0100

    Fixed more bugs, added building total, fixed index generator to ignore some bad .json files

--- scripts/indexgenerator.py
+++ scripts/indexgenerator.py
@@ -44,63 +44,68 @@ else:
                 readfile = open(os.path.join(dirname, filename), 'r').read()
                 jsonfile = json.loads(readfile)
                 
-                alreadyfound = False
-                #if we know about this sensor, add its readings only.
-                labelsplit = jsonfile['label'].split(' ', 1)
-                
-                temptotal = 0.0
-                mostrecentreading = False
-                
-                if labelsplit[1] == recentsensor:
-                    mostrecentreading = True
-                    for i in jsonfile['data']:
-                        temptotal += i[1]
-                    datasize = len(jsonfile['data'])
-                
-                for sensor in elec:
-                    if debug:
-                        print 'trying sensor ID:'+sensor['sensor']
-                    if sensor['sensor'] == 'S-m' + labelsplit[0]:
-                        sensor['readings'].append(filename)
-                        if mostrecentreading:
-                            sensor['recenttotal'] = temptotal
-                            sensor['datasize'] = datasize
-                            sensorlabel = 'S-m' + labelsplit[0]
-                            sensor['sensor'] = sensorlabel
-                            sensor['path'] = jsonfile['path'].lstrip("meters.cl.cam.ac.uk")+"/"+sensorlabel+"-"
-                            sensor['room'] = jsonfile['room']           
-                            sensor['description'] = jsonfile['description']
+                if 'room' and 'path' and 'label' and 'description' and 'data' in jsonfile and len(jsonfile['data']) > 0:
+                    
+                    alreadyfound = False
+                    #if we know about this sensor, add its readings only.
+                    labelsplit = jsonfile['label'].split(' ', 1)
+                    
+                    temptotal = 0.0
+                    mostrecentreading = False
+                    
+                    if labelsplit[1] == recentsensor:
+                        mostrecentreading = True
+                        for i in jsonfile['data']:
+                            temptotal += i[1]
+                        datasize = len(jsonfile['data'])
+                 
+                    for sensor in elec:
                         if debug:
-                            print 'Sensor ID:'+sensor['sensor']+' already found, readings: '+str(sensor['readings'])
-                        alreadyfound = True
-                        break
-                
-                if not alreadyfound:
-                    if debug:
-                        print 'sensor '+'S-m' + str(jsonfile['label'].split(' ', 1)[0])+' not found, adding' 
-                    #if this is a new sensor, add its readings to elec.
-                    tempsensor = {}
-                    tempsensor['readings'] = [filename]
-                    if mostrecentreading:
-                        tempsensor['recenttotal'] = temptotal
-                        tempsensor['datasize'] = datasize
+                            print 'trying sensor ID:'+sensor['sensor']
+                        if 'sensor' in sensor:
+                            if sensor['sensor'] == 'S-m' + labelsplit[0]:
+                                sensor['readings'].append(filename)
+                                if mostrecentreading:
+                                    sensor['recenttotal'] = temptotal
+                                    sensor['datasize'] = datasize
+                                    sensorlabel = 'S-m' + labelsplit[0]
+                                    sensor['sensor'] = sensorlabel
+                                    sensor['path'] = jsonfile['path'].lstrip("meters.cl.cam.ac.uk")+"/"+sensorlabel+"-"
+                                    sensor['room'] = jsonfile['room']           
+                                    sensor['description'] = jsonfile['description']
+                                if debug:
+                                    print 'Sensor ID:'+sensor['sensor']+' already found, readings: '+str(sensor['readings'])
+                                alreadyfound = True
+                                break
+                    
+                    if not alreadyfound:
+                        if debug:
+                            print 'sensor '+'S-m' + str(jsonfile['label'].split(' ', 1)[0])+' not found, adding' 
+                        #if this is a new sensor, add its readings to elec.
+                        tempsensor = {}
+                        tempsensor['readings'] = [filename]
                         sensorlabel = 'S-m' + labelsplit[0]
                         tempsensor['sensor'] = sensorlabel
-                        tempsensor['path'] = jsonfile['path'].lstrip("meters.cl.cam.ac.uk")+"/"+sensorlabel+"-"
-                        tempsensor['room'] = jsonfile['room']           
-                        tempsensor['description'] = jsonfile['description']
-                    elec.append(tempsensor)
+                        if mostrecentreading:
+                            tempsensor['recenttotal'] = temptotal
+                            tempsensor['datasize'] = datasize
+                            tempsensor['path'] = jsonfile['path'].lstrip("meters.cl.cam.ac.uk")+"/"+sensorlabel+"-"
+                            tempsensor['room'] = jsonfile['room']           
+                            tempsensor['description'] = jsonfile['description']
+                        elec.append(tempsensor)
+                        if debug:
+                            print 'New sensor found: '+str(tempsensor)
+                        tempsensor = {}
+                    
+                    
+                    
+                    #for sensor in elec:
+                    #        print sensor['sensor']
+                    
+                    totalfiles = totalfiles + 1
+                else:
                     if debug:
-                        print 'New sensor found: '+str(tempsensor)
-                    tempsensor = {}
-                
-                
-                        
-                #for sensor in elec:
-                #        print sensor['sensor']
-                
-                totalfiles = totalfiles + 1
-                
+                        print "skipped malformed or null file"+str(filename)
     myjson = {}
     sensors = {}
     sensors['elec'] = elec
