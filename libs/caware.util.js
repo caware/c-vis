24951800f79575cf9fc66029a3134f563fa4ad60
Chris Elsmore <elsmorian@gmail.com>
2011-06-06 13:55:49 +0100

    Added cookie and display notification text regarding Alpha status

--- libs/caware.util.js
+++ libs/caware.util.js
@@ -62,3 +62,57 @@ function IntValueStore() {
         this.index = new Array();
     };
 }
+
+function createCookie(name,value,days){
+   // new Date object created
+   var date = new Date();
+   // adding days to current time
+   date.setTime(date.getTime()+(days*24*60*60*1000));
+   // converting to standard format
+   var expires = date.toGMTString();
+   // adding cookie name,value and expiry time
+   document.cookie = name+"="+value+"; expires="+expires+"; path=/";
+}
+
+function readCookie(name){
+  var flag = 0;
+   // getting the browser cookie values into an array
+  var dcmntCookie = document.cookie.split(';');
+  // looping over the array to find our cookie
+  for(var i=0;i < dcmntCookie.length;i++)
+  {
+    var ck = dcmntCookie[i];
+    // loop for removing extra spaces from the beginning of each cookie
+    while (ck.charAt(0)==' ')
+    {
+       ck = ck.substring(1,ck.length);
+    }
+    if(ck)
+    {
+      // splitting the cookie into its name and value
+      cparts = ck.split('=');
+      // setting the flag if a cookie with the name specified exists
+      if (cparts[0]==name){
+        console.log(ck);
+        flag=1;
+     }
+    }
+  }
+  // returning true if cookie exists else returning false
+  if(flag) return true;
+  else return false;
+}
+
+function checkCookie(name){
+  if(readCookie(name))
+  {
+     // hide the div element using css style attribute
+     $("#firstvisit").remove();
+     console.log("emptied top bar vistor welcome back");
+  }
+  // create a new cookie if cookie already not present
+  else{
+    createCookie(name,"VisitedIn28Days",28);
+    console.log("created cookie");
+  }
+}
